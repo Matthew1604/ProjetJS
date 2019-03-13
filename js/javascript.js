928c7e53d46a23f186e9a8b684f3a0fbed296a61
matthew1604 <32646670+matthew1604@users.noreply.github.com>
2019-03-13 23:34:16 +0100

    Test animation

--- js/javascript.js
+++ js/javascript.js
@@ -7,6 +7,8 @@ var ratio = window.devicePixelRatio;
 var sprite = new Image();
 sprite.onload = animate;
 sprite.src = "img/sprite.png";
+var step = 0;
+var prevStep = 0;
 
 canvas.width = w;
 canvas.height = h;
@@ -16,22 +18,52 @@ context.imageSmoothingEnabled = false;
 //context.fillStyle = "green";
 //context.fillRect(10, 20, 100, 200);
 
+var compt = 0;
 function animate() {
-    draw();
-    update();
-    requestAnimationFrame(animate);
+    if (compt < 500) {
+        draw();
+        update(0.07);
+        requestAnimationFrame(animate);
+        compt++;
+    }
 }
 
 function draw() {
     context.clearRect(0, 0, w, h);
-    drawSprite(w/2, h, 150);
+    drawSprite(w/2, h, 200);
 }
 
 function drawSprite(x, y, scale) {
     var s = scale/23;
-    context.drawImage(sprite, 0, 26, 23, 23, x-11*s, y-23*s, 23*s, 23*s);
+    context.drawImage(sprite, Math.floor(step)*25, 25, 23, 24, x-11*s, y-24*s, 24*s, 24*s);
 }
 
-function update() {
-    
+function update(vitesse) {
+
+    if (Math.floor(step) == 0) {
+        if (prevStep == 0) {
+            step += vitesse;
+        } else {
+            step -= vitesse;
+        }
+
+        if (step <= 0) {
+            step = 1;
+            prevStep = 0;
+        }
+    } else {
+        if (Math.floor(step) == 1 || Math.floor(step) == 2) {
+            if (prevStep == 0) {
+                step += vitesse;
+            }
+            else {
+                step -= vitesse;
+            }
+        }
+    }
+
+    if (Math.floor(step) == 3) {
+        prevStep = 2;
+        step = 2 ;
+    }
 }
